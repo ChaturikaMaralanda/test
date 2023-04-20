e1dc0558f0504094bd2bc3e7e9ea2724b84da6e8
parakrama maralanda <parakrama.m@s2t.ai>
2023-04-20 15:43:15 +0530

    Changes for new repo

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,10 +1,26 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path: '',
+    loadChildren: () =>
+      import('./modules/admin-dashboard/admin-dashboard.module').then(
+        (m) => m.AdminDashboardModule
+      ),
+    data: { title: 'Dashboard' },
+  },
+
+  {
+    path: 'stock',
+    loadChildren: () =>
+      import('./modules/stock/stock.module').then((m) => m.StockModule),
+    data: { title: 'stock' },
+  },
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
-  exports: [RouterModule]
+  exports: [RouterModule],
 })
-export class AppRoutingModule { }
+export class AppRoutingModule {}
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,24 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HeaderComponent } from './core/header/header.component';
+import { LeftMenuComponent } from './core/left-menu/left-menu.component';
+import { TopMenuComponent } from './core/top-menu/top-menu.component';
+import { FooterComponent } from './core/footer/footer.component';
+import { RightMenuComponent } from './core/right-menu/right-menu.component';
+import { NavigationComponent } from './core/navigation/navigation.component';
+import { PageHeaderComponent } from './core/page-header/page-header.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    LeftMenuComponent,
+    TopMenuComponent,
+    FooterComponent,
+    RightMenuComponent,
+    NavigationComponent,
+    PageHeaderComponent
   ],
   imports: [
     BrowserModule,
